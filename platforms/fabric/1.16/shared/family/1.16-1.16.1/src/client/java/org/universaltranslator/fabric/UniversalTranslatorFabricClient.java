866f614e1e17af5b3da68334a0c7da3b2673e2cc
wuxiangdan96-byte <wuxiangdan96@gmail.com>
2026-08-16 11:20:50 +0800

    Fix Fabric 1.16 CI build

--- platforms/fabric/1.16/shared/family/1.16-1.16.1/src/client/java/org/universaltranslator/fabric/UniversalTranslatorFabricClient.java
+++ platforms/fabric/1.16/shared/family/1.16-1.16.1/src/client/java/org/universaltranslator/fabric/UniversalTranslatorFabricClient.java
@@ -1,8 +1,7 @@
 package org.universaltranslator.fabric;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
-import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.event.client.ClientTickCallback;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
@@ -49,7 +48,7 @@ public final class UniversalTranslatorFabricClient implements ClientModInitializ
             LOGGER.error("MC Auto Translation Tool configuration failed; translation remains disabled", exception);
             FabricTranslationRuntime.shutdown();
         }
-        ClientTickEvents.END_CLIENT_TICK.register(client -> {
+        ClientTickCallback.EVENT.register(client -> {
             boolean connected = client.world != null && client.getNetworkHandler() != null;
             if (connected && !connectedLastTick) {
                 joinHintTicks = 60;
@@ -107,7 +106,6 @@ public final class UniversalTranslatorFabricClient implements ClientModInitializ
                 }
             }
         });
-        ClientLifecycleEvents.CLIENT_STOPPING.register(client -> FabricTranslationRuntime.shutdown());
     }
 
     public static boolean interceptOutgoingMessage(String message) {
